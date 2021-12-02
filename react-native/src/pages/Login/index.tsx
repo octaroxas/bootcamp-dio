834ebef9577208f3c398d4dff6ad0111a4f5c102
Octacilio C. de Almeida <octa.oca44@gmail.com>
2021-12-02 12:56:57 -0300

    Linhas desnecessarias

--- react-native/src/pages/Login/index.tsx
+++ react-native/src/pages/Login/index.tsx
@@ -10,10 +10,8 @@ export default function Login() {
 
     const { register, handleSubmit, setValue } = useForm()
 
-    const [form, setForm] = useState("")
-
     function sub(data) {
-        setForm(`senha: ${data.senha} email: ${data.email}`)
+        console.log(`senha: ${data.senha} email: ${data.email}`)
     }
 
     useEffect(() => {
@@ -25,22 +23,19 @@ export default function Login() {
         <SafeAreaView style={styles.container}>
             <Image style={styles.image} source={v} />
             <View style={styles.formContainer}>
+
                 <Text style={styles.title}>
                     Login
                 </Text>
 
                 <Input placeholder="E-mail" label="E-mail" setValue={(entrada) => setValue('email', entrada)} />
                 <Input placeholder="Senha" label="Senha" setValue={(entrada) => setValue('senha', entrada)} />
-
                 <Button onPress={handleSubmit(sub)}>Entrar</Button>
 
                 <View style={styles.linksContainer}>
                     <Text style={styles.link}>Esqueci a senha</Text>
                     <Text style={styles.link}>Não tenho cadastro</Text>
                 </View>
-                <Text style={{ backgroundColor: 'grey', padding: 20, color: '#fff' }}>
-                    {form}
-                </Text>
             </View>
         </SafeAreaView>
     )
